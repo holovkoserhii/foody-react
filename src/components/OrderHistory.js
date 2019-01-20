a58333917451f3281c9c334fdda45b48c54f59a0
Serhii <holovko.s@gmail.com>
2019-01-20 14:54:39 +0200

    homework-2 done

--- src/components/OrderHistory.js
+++ src/components/OrderHistory.js
@@ -2,28 +2,31 @@ import React, { Component } from 'react';
 import orderHistory from '../order-history.json';
 import TableRow from './order-history/TableRow';
 import AddToHistory from './order-history/AddToHistory';
-import Spinner from "./Spinner";
-import shortid from "shortid";
+import Spinner from './Spinner';
+import shortid from 'shortid';
 
 export default class OrderHistory extends Component {
   state = {
     orders: [],
+    isLoading: false,
   };
 
   componentDidMount() {
-    this.setState({ isLoading: true });
-    this.getOrders();
+    this.setState(prevState =>
+      Object.assign({}, prevState, { isLoading: true }),
+    );
+    this.getOrders(); //here would be async.function instead. When finished - hide Spinner
   }
 
-  getOrders = () => this.setState({ orders: orderHistory });
+  getOrders = () => this.setState({ orders: orderHistory, isLoading: false });
 
   handleDeleteOrderFromHistory = id => {
     const newOrderHistory = this.state.orders.filter(item => item.id !== id);
     this.setState({ orders: newOrderHistory });
   };
 
-  handleAddToHistory = (obj) => {
-    const newObj = Object.assign(obj, {id: shortid.generate()});
+  handleAddToHistory = obj => {
+    const newObj = Object.assign(obj, { id: shortid.generate() });
     this.setState(prevState => ({ orders: [...prevState.orders, newObj] }));
   };
 
@@ -32,27 +35,32 @@ export default class OrderHistory extends Component {
     return (
       <>
         <AddToHistory onAdd={this.handleAddToHistory} />
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Price</th>
-              <th>Delivery address</th>
-              <th>Rating</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders &&
-              orders.map(order => {
-                return (
-                  <TableRow key={order.id}
-                    item={order}
-                    deleteRow={this.handleDeleteOrderFromHistory}
-                  />
-                )
-              })}
-          </tbody>
-        </table>
+        {this.isLoading ? (
+          <Spinner />
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Price</th>
+                <th>Delivery address</th>
+                <th>Rating</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders &&
+                orders.map(order => {
+                  return (
+                    <TableRow
+                      key={order.id}
+                      item={order}
+                      deleteRow={this.handleDeleteOrderFromHistory}
+                    />
+                  );
+                })}
+            </tbody>
+          </table>
+        )}
       </>
     );
   }
